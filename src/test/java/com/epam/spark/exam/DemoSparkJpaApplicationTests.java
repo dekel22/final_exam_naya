0f5125285d66aa6a8a34e096da96a357b5b6c361
dekel22 <dekell8@gmail.com>
2021-06-16 16:06:05 +0300

    Initial commit

---
+++ src/test/java/com/epam/spark/exam/DemoSparkJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.spark.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSparkJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
